16f75ec897ca2cff07335427cac62025560cb782
Aria Zhang <zmzhangg@gmail.com>
2023-03-23 21:17:53 -0400

    initial updates

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-app.get("/",function(req, res){
-  // console.log(request);
-  res.send("bybye");
-});
+passport.use(new GoogleStrategy());//create a new instance
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
+
+
+
+console.log(`Server Running at port ${PORT}  CNTL-C to quit`)
+console.log(`To Test:`)
+console.log(`Open several browsers to: http://localhost:${PORT}`)
