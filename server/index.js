c2d057cc9b6d9fa80811a3862cc6a3aed15a897f
Aria Zhang <zmzhangg@gmail.com>
2023-03-27 22:39:03 -0400

    complete authentication

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const mongoose = require('mongoose');
-
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
-mongoose.connect();
+mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24* 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());  
 
 //call the function with app parameter
 require('./routes/authRoutes')(app);
@@ -20,3 +33,4 @@ console.log(`Server Running at port ${PORT}  CNTL-C to quit`)
 console.log(`To Test:`)
 console.log(`Open several browsers to: http://localhost:${PORT}`)
 console.log(`Open several browsers to: http://localhost:${PORT}/auth/google`)
+console.log(`Open several browsers to: http://localhost:${PORT}/api/current_user`)
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -14,5 +14,14 @@ module.exports = app => {
         '/auth/google/callback', 
         passport.authenticate('google')//passport will see the code inside the URL
     );//with the name google, use the strategy above 'GoogleStrategy'
-}
+
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    });
+
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    });
+};
   
